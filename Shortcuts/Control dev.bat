f3a77ba267d1c993f93910850a8ea8213d0d1b5b
Ashwin Nair <ashwinnair2011@gmail.com>
2017-02-13 12:02:19 +1100

    First Commit

---
+++ Shortcuts/Control dev.bat
@@ -0,0 +1,6 @@
+REM Git bash, gui and gitk pointing to target
+"Dev\OrgTimer GIT GUI.lnk"
+"Dev\OrgTimer GIT k.lnk"
+"Dev\OrgTimer Git Bash.lnk"
+REM VS solution
+"C:\Users\ashwinnair\Source\Repos\OrgTimer\OrgTimer\OrgTimer.sln"
