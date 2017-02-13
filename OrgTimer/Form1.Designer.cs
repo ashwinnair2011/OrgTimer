cd62b406c612f63ff8d9b63373b814f79d280a13
Ashwin Nair <ashwinnair2011@gmail.com>
2017-02-13 14:14:26 +1100

    Implemented basic timer for 10+2 organization

--- OrgTimer/Form1.Designer.cs
+++ OrgTimer/Form1.Designer.cs
@@ -29,11 +29,61 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ActionButton = new System.Windows.Forms.Button();
+            this.JobTimer = new System.Windows.Forms.Timer(this.components);
+            this.OrgTimerNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // ActionButton
+            // 
+            this.ActionButton.BackColor = System.Drawing.Color.SeaShell;
+            this.ActionButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActionButton.Location = new System.Drawing.Point(2, 1);
+            this.ActionButton.Name = "ActionButton";
+            this.ActionButton.Size = new System.Drawing.Size(245, 65);
+            this.ActionButton.TabIndex = 0;
+            this.ActionButton.Tag = "";
+            this.ActionButton.Text = "Start";
+            this.ActionButton.UseVisualStyleBackColor = false;
+            this.ActionButton.Click += new System.EventHandler(this.ActionButton_Click);
+            // 
+            // JobTimer
+            // 
+            this.JobTimer.Interval = 1000;
+            this.JobTimer.Tick += new System.EventHandler(this.JobTimer_Tick);
+            // 
+            // OrgTimerNotify
+            // 
+            this.OrgTimerNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("OrgTimerNotify.Icon")));
+            this.OrgTimerNotify.Text = "Org Timer";
+            this.OrgTimerNotify.Visible = true;
+            this.OrgTimerNotify.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.OrgTimerNotify_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(249, 68);
+            this.Controls.Add(this.ActionButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.75D;
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ActionButton;
+        private System.Windows.Forms.Timer JobTimer;
+        private System.Windows.Forms.NotifyIcon OrgTimerNotify;
     }
 }
 
--- OrgTimer/Form1.cs
+++ OrgTimer/Form1.cs
@@ -1,20 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OrgTimer
 {
     public partial class Form1 : Form
     {
+        enum TimerState
+        {
+            Working,
+            Chilling,
+            Stopped
+        }
+        const int WorkIntervalInSecs = 600;
+        const int ChillIntervalInSecs = 120;
+        int _currentIntervalTimeInSecs;
+        TimerState _timerState;
+
         public Form1()
         {
             InitializeComponent();
+            _timerState = TimerState.Stopped;
+        }
+
+        private void ActionButton_Click(object sender, EventArgs e)
+        {
+            switch (_timerState)
+            {
+                case TimerState.Stopped:
+                case TimerState.Chilling:
+                    //start timer for 10 minutes
+                    ActionButton.BackColor = Color.SeaShell;
+                    _timerState = TimerState.Working;
+                    _currentIntervalTimeInSecs = WorkIntervalInSecs;
+                    JobTimer.Start();
+                    break;
+                case TimerState.Working:
+                    //start timer for 2 minutes
+                    _timerState = TimerState.Chilling;
+                    _currentIntervalTimeInSecs = ChillIntervalInSecs;
+                    break;
+            }
+        }
+
+        private void JobTimer_Tick(object sender, EventArgs e)
+        {
+            JobTimer.Stop();
+            string timeLeft = TimeSpan.FromSeconds(--_currentIntervalTimeInSecs).ToString(@"mm\:ss");
+            ActionButton.Text = _timerState.ToString() + " " + timeLeft;
+            if (_currentIntervalTimeInSecs == 0)
+            {
+                _timerState = TimerState.Stopped;
+                ActionButton.BackColor = Color.Red;
+                return;
+            }
+            JobTimer.Start();
+        }
+
+        private void OrgTimerNotify_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
         }
     }
 }
