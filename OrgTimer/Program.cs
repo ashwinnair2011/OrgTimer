80620686a387b5ec94fc470032cf3e45ecb33f07
ashwinnair2011 <anair@liveperson.com>
2017-02-21 22:14:14 +1100

    Only allow single instance of application #4

--- OrgTimer/Program.cs
+++ OrgTimer/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace OrgTimer
 {
     static class Program
     {
+        static readonly Mutex SingleInstanceMutex = new Mutex(false, "18a6d808-bd0b-4e86-85d0-76da809e9289");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new OrgTimerForm());
+            if (SingleInstanceMutex.WaitOne(TimeSpan.Zero, true))
+            {
+                try
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new OrgTimerForm());
+                }
+                finally
+                {
+                    SingleInstanceMutex.ReleaseMutex();
+                }
+            }
+            else
+            {
+                MessageBox.Show("An instance of the application is already running.");
+            }
         }
     }
 }
